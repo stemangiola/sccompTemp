99dbbf302eba1bd351458cd7ff3473520a8548eb
stemangiola <mangiolastefano@gmail.com>
2021-06-06 11:31:31 +1000

    first commit

---
+++ R/sccompTemp-package.R
@@ -0,0 +1,16 @@
+#' The 'sccompTemp' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name sccompTemp-package
+#' @aliases sccompTemp
+#' @useDynLib sccompTemp, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
+NULL
